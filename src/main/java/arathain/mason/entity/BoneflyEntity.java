9194a780147b10bc83f954c9f89a2c2eeb0273a7
BlahBlahBlahBlahBlahBlah <156343313+PunchmadeDav@users.noreply.github.com>
2025-03-04 20:47:52 +0000

    Update BoneflyEntity.java

--- src/main/java/arathain/mason/entity/BoneflyEntity.java
+++ src/main/java/arathain/mason/entity/BoneflyEntity.java
@@ -7,7 +7,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.TargetPredicate;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
