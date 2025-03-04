d32747ca3eaf8a2b62beb83f2fc29b7103f2cbeb
BlahBlahBlahBlahBlahBlah <156343313+PunchmadeDav@users.noreply.github.com>
2025-03-04 20:48:28 +0000

    Update BoneflyEntityModel.java

--- src/main/java/arathain/mason/client/BoneflyEntityModel.java
+++ src/main/java/arathain/mason/client/BoneflyEntityModel.java
@@ -1,14 +1,11 @@
 package arathain.mason.client;
 
 import arathain.mason.entity.BoneflyEntity;
-import arathain.mason.init.MasonObjects;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
 import software.bernie.geckolib.constant.DataTickets;
 import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
 import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
-import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.model.data.EntityModelData;
 
 
