2d5718b11ac917c2fdc8d261ed10c49120a356e2
BlahBlahBlahBlahBlahBlah <156343313+PunchmadeDav@users.noreply.github.com>
2025-03-04 20:49:45 +0000

    Update MerchantSimulacrumBlock.java

--- src/main/java/arathain/mason/item/MerchantSimulacrumBlock.java
+++ src/main/java/arathain/mason/item/MerchantSimulacrumBlock.java
@@ -1,25 +1,20 @@
 package arathain.mason.item;
 
 import arathain.mason.entity.SoulmouldEntity;
-import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.Waterloggable;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.boss.WitherEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.registry.tag.FluidTags;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
