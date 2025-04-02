a1c6e2ab43e351ea785b5fb55479d14137f30ad6
BlahBlahBlahBlahBlahBlah <156343313+PunchmadeDav@users.noreply.github.com>
2025-04-02 13:09:00 -0500

    Update MinecraftClientMixin.java

--- src/main/java/arathain/mason/mixin/MinecraftClientMixin.java
+++ src/main/java/arathain/mason/mixin/MinecraftClientMixin.java
@@ -12,9 +12,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import javax.annotation.Nullable;
-
+import org.jetbrains.annotations.Nullable;
 
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
