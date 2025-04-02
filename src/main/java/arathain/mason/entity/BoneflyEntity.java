f33491d6f320f7cd0c56feb700c527eae7030ff0
BlahBlahBlahBlahBlahBlah <156343313+PunchmadeDav@users.noreply.github.com>
2025-04-02 13:02:13 -0500

    Update BoneflyEntity.java

--- src/main/java/arathain/mason/entity/BoneflyEntity.java
+++ src/main/java/arathain/mason/entity/BoneflyEntity.java
@@ -40,8 +40,8 @@ import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.core.animation.RawAnimation;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
